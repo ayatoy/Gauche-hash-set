12ae71e1364e5f71eeadef16d5d6754d8d7da68e
ayatoy <ayatoy@gmail.com>
2012-04-20 15:13:04 +0900

    基本的な手続きを追加

--- hash-set.scm
+++ hash-set.scm
@@ -1,3 +1,187 @@
 (define-module hash-set
-  (export ))
+  (use gauche.record)
+  (export <hash-set>
+          hash-set?
+          hash-set-table
+          make-hash-set
+          hash-set-type
+          hash-set-num-entries
+          hash-set
+          hash-set->list
+          list->hash-set
+          hash-set-exists?
+          hash-set-delete!
+          hash-set-clear!
+          hash-set<=
+          hash-set=
+          hash-set-adjoin
+          hash-set-adjoin!
+          hash-set-union
+          hash-set-union!
+          hash-set-intersection
+          hash-set-intersection!
+          hash-set-difference
+          hash-set-difference!
+          hash-set-xor
+          hash-set-xor!
+          hash-set-for-each
+          hash-set-map
+          hash-set-fold))
 (select-module hash-set)
+
+(define-record-type <hash-set> %make-hash-set hash-set?
+  (table hash-set-table))
+
+(define-method write-object ((hs <hash-set>) oport)
+  (format oport "#<hash-set ~s>" (hash-set-type hs)))
+
+(define (make-hash-set :optional (type 'eq?))
+  (%make-hash-set (make-hash-table type)))
+
+(define (hash-set-type hs)
+  (hash-table-type (hash-set-table hs)))
+
+(define (hash-set-num-entries hs)
+  (hash-table-num-entries (hash-set-table hs)))
+
+(define (hash-set type . els)
+  (%make-hash-set
+   (rlet1 ht (make-hash-table type)
+     (for-each (^[el] (hash-table-put! ht el (undefined))) els))))
+
+(define (hash-set->list hs)
+  (hash-table-keys (hash-set-table hs)))
+
+(define (list->hash-set lst :optional (type 'eq?))
+  (apply hash-set type lst))
+
+(define (hash-set-exists? hs el)
+  (hash-table-exists? (hash-set-table hs) el))
+
+(define (hash-set-delete! hs el)
+  (hash-table-delete! (hash-set-table hs) el))
+
+(define (hash-set-clear! hs)
+  (hash-table-clear! (hash-set-table hs)))
+
+(define (hash-set<= . hsets)
+  (or (>= 1 (length hsets))
+      (and (let1 ht (hash-set-table (cadr hsets))
+             (let loop ([ks (hash-table-keys (hash-set-table (car hsets)))])
+               (or (null? ks)
+                   (and (hash-table-exists? ht (car ks))
+                        (loop (cdr ks))))))
+           (apply hash-set<= (cdr hsets)))))
+
+(define (hash-set= . hsets)
+  (let1 s (car hsets)
+    (let loop ([rest (cdr hsets)])
+      (or (null? rest)
+          (and (let1 s1 (car rest)
+                 (and (hash-set<= s s1) (hash-set<= s1 s)))
+               (loop (cdr rest)))))))
+
+(define (hash-set-adjoin hs . els)
+  (let* ([ht  (hash-set-table hs)]
+         [ht1 (make-hash-table (hash-table-type ht))])
+    (hash-table-for-each ht (^[k v] (hash-table-put! ht1 k (undefined))))
+    (for-each (^[el] (hash-table-put! ht1 el (undefined))) els)
+    (%make-hash-set ht1)))
+
+(define (hash-set-adjoin! hs . els)
+  (let1 ht (hash-set-table hs)
+    (for-each (^[el] (hash-table-put! ht el (undefined))) els))
+  hs)
+
+(define (hash-set-union . hsets)
+  (let* ([ht  (hash-set-table (car hsets))]
+         [ht1 (make-hash-table (hash-table-type ht))])
+    (hash-table-for-each ht (^[k v] (hash-table-put! ht1 k (undefined))))
+    (for-each (^[hs] (hash-table-for-each
+                      (hash-set-table hs)
+                      (^[k v] (hash-table-put! ht1 k (undefined)))))
+              (cdr hsets))
+    (%make-hash-set ht1)))
+
+(define (hash-set-union! . hsets)
+  (rlet1 hs (car hsets)
+    (let1 ht (hash-set-table hs)
+      (for-each (^[hs] (hash-table-for-each
+                        (hash-set-table hs)
+                        (^[k v] (hash-table-put! ht k (undefined)))))
+                (cdr hsets)))))
+
+(define (hash-set-intersection . hsets)
+  (let* ([ht  (hash-set-table (car hsets))]
+         [ht1 (make-hash-table (hash-table-type ht))])
+    (hash-table-for-each ht (^[k v] (hash-table-put! ht1 k (undefined))))
+    (for-each (^[hs] (let1 ht (hash-set-table hs)
+                       (hash-table-for-each
+                        ht1
+                        (^[k v] (unless (hash-table-exists? ht k)
+                                  (hash-table-delete! ht1 k))))))
+              (cdr hsets))
+    (%make-hash-set ht1)))
+
+(define (hash-set-intersection! . hsets)
+  (rlet1 hs (car hsets)
+    (let1 ht (hash-set-table hs)
+      (for-each (^[hs] (let1 ht1 (hash-set-table hs)
+                         (hash-table-for-each
+                          ht
+                          (^[k v] (unless (hash-table-exists? ht1 k)
+                                    (hash-table-delete! ht k))))))
+                (cdr hsets)))))
+
+(define (hash-set-difference . hsets)
+  (let* ([ht  (hash-set-table (car hsets))]
+         [ht1 (make-hash-table (hash-table-type ht))])
+    (hash-table-for-each ht (^[k v] (hash-table-put! ht1 k (undefined))))
+    (for-each (^[hs] (let1 ht (hash-set-table hs)
+                       (hash-table-for-each
+                        ht1
+                        (^[k v] (when (hash-table-exists? ht k)
+                                  (hash-table-delete! ht1 k))))))
+              (cdr hsets))
+    (%make-hash-set ht1)))
+
+(define (hash-set-difference! . hsets)
+  (rlet1 hs (car hsets)
+    (let1 ht (hash-set-table hs)
+      (for-each (^[hs] (let1 ht1 (hash-set-table hs)
+                         (hash-table-for-each
+                          ht
+                          (^[k v] (when (hash-table-exists? ht1 k)
+                                    (hash-table-delete! ht k))))))
+              (cdr hsets)))))
+
+(define (hash-set-xor . hsets)
+  (let* ([ht  (hash-set-table (car hsets))]
+         [ht1 (make-hash-table (hash-table-type ht))])
+    (hash-table-for-each ht (^[k v] (hash-table-put! ht1 k (undefined))))
+    (for-each (^[hs] (hash-table-for-each
+                      (hash-set-table hs)
+                      (^[k v] (if (hash-table-exists? ht1 k)
+                                (hash-table-delete! ht1 k)
+                                (hash-table-put! ht1 k (undefined))))))
+              (cdr hsets))
+    (%make-hash-set ht1)))
+
+(define (hash-set-xor! . hsets)
+  (rlet1 hs (car hsets)
+    (let1 ht (hash-set-table hs)
+      (for-each (^[hs] (hash-table-for-each
+                        (hash-set-table hs)
+                        (^[k v] (if (hash-table-exists? ht k)
+                                  (hash-table-delete! ht k)
+                                  (hash-table-put! ht k (undefined))))))
+              (cdr hsets)))))
+
+(define (hash-set-for-each hs proc)
+  (hash-table-for-each (hash-set-table hs) (^[k v] (proc k))))
+
+(define (hash-set-map hs proc)
+  (hash-table-map (hash-set-table hs) (^[k v] (proc k))))
+
+(define (hash-set-fold hs kons knil)
+  (hash-table-fold (hash-set-table hs) (^[k v ac] (kons k ac)) knil))
--- test.scm
+++ test.scm
@@ -1,7 +1,187 @@
 (use gauche.test)
 
 (test-start "hash-set")
+
 (use hash-set)
 (test-module 'hash-set)
+
+(test* "make-hash-set" <hash-set>
+       (class-of (make-hash-set)))
+
+(test* "hash-set" <hash-set>
+       (class-of (hash-set 'eqv?)))
+
+(test* "hash-set?" #t
+       (hash-set? (make-hash-set)))
+
+(test* "hash-set-table" #t
+       (hash-table? (hash-set-table (make-hash-set))))
+
+(test* "hash-set-type" 'equal?
+       (hash-set-type (make-hash-set 'equal?)))
+
+(test* "hash-set-num-entries" 3
+       (hash-set-num-entries (hash-set 'eqv? 1 2 3)))
+
+(test* "hash-set->list" #t
+       (let1 lst (hash-set->list (hash-set 'eqv? 1 2 3))
+         (and (= 3 (length lst))
+              (member 1 lst)
+              (member 2 lst)
+              (member 3 lst)
+              #t)))
+
+(test* "list->hash-set" #t
+       (let1 lst (hash-set->list (list->hash-set '(1 2 3) 'eqv?))
+         (and (= 3 (length lst))
+              (member 1 lst)
+              (member 2 lst)
+              (member 3 lst)
+              #t)))
+
+(test* "hash-set-exists?" #t
+       (let1 hs (hash-set 'eqv? 1)
+         (and (hash-set-exists? hs 1)
+              (not (hash-set-exists? hs 2)))))
+
+(test* "hash-set-delete!" '()
+       (let1 hs (hash-set 'eqv? 1)
+         (hash-set-delete! hs 1)
+         (hash-set->list hs)))
+
+(test* "hash-set-clear!" '()
+       (let1 hs (hash-set 'eqv? 1 2 3)
+         (hash-set-clear! hs)
+         (hash-set->list hs)))
+
+(test* "hash-set<= 1" #t
+       (hash-set<= (hash-set 'eqv? 1)
+                   (hash-set 'eqv? 1 2)
+                   (hash-set 'eqv? 1 2 3)))
+
+(test* "hash-set<= 2" #f
+       (hash-set<= (hash-set 'eqv? 1)
+                   (hash-set 'eqv? 1 2 3)
+                   (hash-set 'eqv? 1 2)))
+
+(test* "hash-set= 1" #t
+       (hash-set= (hash-set 'eqv? 1)
+                  (hash-set 'eqv? 1)
+                  (hash-set 'eqv? 1)))
+
+(test* "hash-set= 2" #f
+       (hash-set= (hash-set 'eqv? 1)
+                  (hash-set 'eqv? 1 2)
+                  (hash-set 'eqv? 1)))
+
+(test* "hash-set-adjoin" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2 3)]
+              [hs2 (hash-set-adjoin hs1 4 5)])
+         (and (= 5 (hash-set-num-entries hs2))
+              (not (eq? hs1 hs2)))))
+
+(test* "hash-set-adjoin!" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2 3)]
+              [hs2 (hash-set-adjoin! hs1 4 5)])
+         (and (= 5 (hash-set-num-entries hs2))
+              (eq? hs1 hs2))))
+
+(test* "hash-set-union" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-union hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (memv 1 lst)
+              (memv 2 lst)
+              (memv 3 lst)
+              (not (eq? hs1 hs3)))))
+
+(test* "hash-set-union!" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-union! hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (memv 1 lst)
+              (memv 2 lst)
+              (memv 3 lst)
+              (eq? hs1 hs3))))
+
+(test* "hash-set-intersection" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-intersection hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (not (memv 1 lst))
+              (memv 2 lst)
+              (not (memv 3 lst))
+              (not (eq? hs1 hs3)))))
+
+(test* "hash-set-intersection!" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-intersection! hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (not (memv 1 lst))
+              (memv 2 lst)
+              (not (memv 3 lst))
+              (eq? hs1 hs3))))
+
+(test* "hash-set-difference" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-difference hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (memv 1 lst)
+              (not (memv 2 lst))
+              (not (memv 3 lst))
+              (not (eq? hs1 hs3)))))
+
+(test* "hash-set-difference!" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-difference! hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (memv 1 lst)
+              (not (memv 2 lst))
+              (not (memv 3 lst))
+              (eq? hs1 hs3))))
+
+(test* "hash-set-xor" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-xor hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (memv 1 lst)
+              (not (memv 2 lst))
+              (memv 3 lst)
+              (not (eq? hs1 hs3)))))
+
+(test* "hash-set-xor!" #t
+       (let* ([hs1 (hash-set 'eqv? 1 2)]
+              [hs2 (hash-set 'eqv? 2 3)]
+              [hs3 (hash-set-xor! hs1 hs2)]
+              [lst (hash-set->list hs3)])
+         (and (memv 1 lst)
+              (not (memv 2 lst))
+              (memv 3 lst)
+              (eq? hs1 hs3))))
+
+(test* "hash-set-for-each" #t
+       (let1 lst '()
+         (hash-set-for-each (hash-set 'eqv? 1 2 3) (^[x] (push! lst x)))
+         (and (memv 1 lst)
+              (memv 2 lst)
+              (memv 3 lst)
+              #t)))
+
+(test* "hash-set-map" #t
+       (let1 lst (hash-set-map (hash-set 'eqv? 1 2 3) (^[x] x))
+         (and (memv 1 lst)
+              (memv 2 lst)
+              (memv 3 lst)
+              #t)))
+
+(test* "hash-set-fold" 6
+       (hash-set-fold (hash-set 'eqv? 1 2 3) + 0))
 
 (test-end)
